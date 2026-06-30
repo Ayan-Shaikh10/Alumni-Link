83d3fc863379767698f7e8b30e02ed341ccd8e1f
Ayan-Shaikh10 <ayan8779282612@gmail.com>
2026-06-30 07:13:17 +0530

    FireBase authentication

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCejmdQhJ8guWXTVMvvBpDPZwuOybcemKw",
+    authDomain: "alumni-link-d6b44.firebaseapp.com",
+    projectId: "alumni-link-d6b44",
+    storageBucket: "alumni-link-d6b44.firebasestorage.app",
+    messagingSenderId: "566048501452",
+    appId: "1:566048501452:web:40a1d76480719dbc36dc69"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
